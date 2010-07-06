6b08d471cd478dc1dfbb6c19bd4fb76ee4d31ee0
Michael Breedlove <michael.breedlove1@gmail.com>
2010-07-06 01:53:29 -0500

    Updated TODO Updated CSS Selector in reddit_image_saver.rb

--- reddit_image_save.rb
+++ reddit_image_save.rb
@@ -13,7 +13,7 @@ require 'uri'
 reddits = ['Jailbait', 'SceneGirls', 'LegalTeens', 'Ass', 'HighHeels', 'RealGirls', 'SexyButNotPorn'] 
 
 # Desired sorting
-sort_type = 'top' # hot, new, controversial, top
+sort_type = 'hot' # hot, new, controversial, top
 
 # Folder to save pictures to
 dir = 'Saved Reddit Pics'
@@ -57,10 +57,10 @@ res = get_page_source(custom_url)
 # Add URLs and Title to hash
 urls = {}
 doc = Hpricot.parse(res.body)
-(doc/'.content'/'#siteTable'/'.thing'/'.entry'/'.title'/:a/'.title').each do |link|
+(doc/'.content'/'#siteTable'/'.thing'/'.unvoted'/'.entry'/'.title'/:a/'.title').each do |link|
   urls[link.inner_text] = link.attributes["href"]
 end
-
+puts urls.inspect
 # Fix ugly imgur URLs
 urls.each_pair do |name, url|
   # imgur.com -> i.imgur.com
